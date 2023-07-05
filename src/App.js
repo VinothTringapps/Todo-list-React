273c95c802aff25de8e77b17583657f76c8f7af9
vinothkumarp <123440655+VinothTringapps@users.noreply.github.com>
2023-07-05 14:58:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ import  Footer  from './Footer';
 import Add from './Add';
 import SearchItem from './SearchItem';
 function App() {
- 
-<<<<<<< HEAD
-  const [items,setItem]=useState( JSON.parse(localStorage.getItem('todo_list')) !== null ?
-=======
   const [items,setItem]=useState(
    JSON.parse(localStorage.getItem('todo_list')) !== null ?
->>>>>>> origin/master
     JSON.parse(localStorage.getItem('todo_list')) : []
   )
   const [del,setDel]=useState('')
